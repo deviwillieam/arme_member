44ffbce78ba0dcf8881e8e21f73e8433670b739a
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-23 12:07:52 +0800

    update

--- resources/views/pages/dashboard-tambah.blade.php
+++ resources/views/pages/dashboard-tambah.blade.php
@@ -7,7 +7,7 @@
 @section('subcontent')
 <div class="field field-name-body field-type-text-with-summary field-label-hidden">
       <div class="field-items">
-         <div class="field-item even" property="content:encoded"><iframe src="https://app.powerbi.com/view?r=eyJrIjoiMzc5ZTdmZDMtYTYyYy00N2Q2LWI0MDktN2FlM2VmMDcwYzU3IiwidCI6ImY2ZjcwZjFiLTJhMmQtNGYzMC04NTJhLTY0YjhjZTBjMTlkNyIsImMiOjF9" frameborder="0" allowfullscreen="true" width="100%" height="600"></iframe></div>
+         <div class="field-item even" property="content:encoded"><iframe src="https://app.powerbi.com/view?r=eyJrIjoiYTU5ZGFjNDAtNmIwNS00ZmRkLTg0NTYtYTJhMzRkOWM4MWFlIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSectionb94a61918628c16bb808" frameborder="0" allowfullscreen="true" width="100%" height="890"></iframe></div>
       </div>
    </div>
    </div>
