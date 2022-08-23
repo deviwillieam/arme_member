d42da906a620ea3098500c29ab1e8a48321e1ab7
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-23 21:55:10 +0800

    update

--- resources/views/pages/dashboard-tambah.blade.php
+++ resources/views/pages/dashboard-tambah.blade.php
@@ -35,9 +35,9 @@ height: 100%;
  <div class="embed-container">
 <iframe src="https://app.powerbi.com/view?r=eyJrIjoiY2MyYmZiMDItYmE1Yi00ZmIxLThmMGUtNDIyYmJjNjE0Y2Y1IiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
 </div>
-<div class="embed-container">
+<!-- <div class="embed-container">
 <iframe src="https://app.powerbi.com/MobileRedirect.html?reportObjectId=4b5435da-c227-4ce3-8c89-1197fa91e060&action=OpenReport&reportPage=ReportSection&bookmarkGuid=c1e2545e-b031-4229-9e56-17aeae718cca&Context=share-report&ctid=0e0db2ad-c416-47c7-88ec-ceac4ee76767&pbi_source=mobile_ios" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
 </div>
-<iframe src="https://app.powerbi.com/MobileRedirect.html?reportObjectId=4b5435da-c227-4ce3-8c89-1197fa91e060&action=OpenReport&reportPage=ReportSection&bookmarkGuid=c1e2545e-b031-4229-9e56-17aeae718cca&Context=share-report&ctid=0e0db2ad-c416-47c7-88ec-ceac4ee76767&pbi_source=mobile_ios"> </iframe>
+<iframe src="https://app.powerbi.com/MobileRedirect.html?reportObjectId=4b5435da-c227-4ce3-8c89-1197fa91e060&action=OpenReport&reportPage=ReportSection&bookmarkGuid=c1e2545e-b031-4229-9e56-17aeae718cca&Context=share-report&ctid=0e0db2ad-c416-47c7-88ec-ceac4ee76767&pbi_source=mobile_ios"> </iframe> -->
 
 @endsection
