b7a6c2c7672333f2c01428fb00aa660c07ee48ea
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-23 12:18:22 +0800

    lagi

--- resources/views/pages/dashboard-tambah.blade.php
+++ resources/views/pages/dashboard-tambah.blade.php
@@ -12,6 +12,19 @@
    </div>
    </div>
  -->
+ <style>.embed-container {
+position: relative;
+padding-bottom: 70.75%;
+height: 0;
+overflow: hidden;
+}
+.embed-container iframe {
+position: absolute;
+top:0;
+left: 0;
+width: 100%;
+height: 100%;
+}</style>
 
  <div class="embed-container">
 <iframe src="https://app.powerbi.com/view?r=eyJrIjoiYTU5ZGFjNDAtNmIwNS00ZmRkLTg0NTYtYTJhMzRkOWM4MWFlIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSectionb94a61918628c16bb808" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
