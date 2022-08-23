92577a11668d66b4941a5a9b70058c7616315fc7
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-23 20:08:53 +0800

    update

--- resources/views/pages/dashboard-tambah.blade.php
+++ resources/views/pages/dashboard-tambah.blade.php
@@ -26,15 +26,15 @@ width: 100%;
 height: 100%;
 }</style>
 
-<!-- 
+
 <div id="Container"
      style="padding-bottom:56.25%; position:relative; display:block; width: 100%">
-     <iframe id="ViostreamIframe" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiYTU5ZGFjNDAtNmIwNS00ZmRkLTg0NTYtYTJhMzRkOWM4MWFlIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSectionb94a61918628c16bb808" frameborder="0" allowfullscreen="" style="position:absolute; top:0; left: 0"></iframe>
-</div> -->
+     <iframe id="ViostreamIframe" width="100%" height="100%" src="
+<iframe title="Analisis Data Selangor - Page 1" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiY2MyYmZiMDItYmE1Yi00ZmIxLThmMGUtNDIyYmJjNjE0Y2Y1IiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+" frameborder="0" allowfullscreen="" style="position:absolute; top:0; left: 0"></iframe>
+</div>
  <!-- <div class="embed-container">
 <iframe src="" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
 </div> -->
-
-<iframe title="Analisis Data Selangor - Page 1" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiY2MyYmZiMDItYmE1Yi00ZmIxLThmMGUtNDIyYmJjNjE0Y2Y1IiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
 
 @endsection
