415151ada2dab38ec3c2781c0b0e06d002340976
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-23 12:17:02 +0800

    update

--- resources/views/pages/dashboard-tambah.blade.php
+++ resources/views/pages/dashboard-tambah.blade.php
@@ -5,10 +5,16 @@
 @endsection
 
 @section('subcontent')
-<div class="field field-name-body field-type-text-with-summary field-label-hidden">
+<!-- <div class="field field-name-body field-type-text-with-summary field-label-hidden">
       <div class="field-items">
          <div class="field-item even" property="content:encoded"><iframe src="https://app.powerbi.com/view?r=eyJrIjoiYTU5ZGFjNDAtNmIwNS00ZmRkLTg0NTYtYTJhMzRkOWM4MWFlIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSectionb94a61918628c16bb808" frameborder="0" allowfullscreen="true" width="100%" height="890"></iframe></div>
       </div>
    </div>
    </div>
+ -->
+
+ <div class="embed-container">
+<iframe src="https://app.powerbi.com/view?r=eyJrIjoiYTU5ZGFjNDAtNmIwNS00ZmRkLTg0NTYtYTJhMzRkOWM4MWFlIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSectionb94a61918628c16bb808" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
+</div>
+
 @endsection
