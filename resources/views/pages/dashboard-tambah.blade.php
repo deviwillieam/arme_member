88be17158c1cca9822a5227cf4c61e233930cd7c
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-23 20:10:33 +0800

    update

--- resources/views/pages/dashboard-tambah.blade.php
+++ resources/views/pages/dashboard-tambah.blade.php
@@ -29,9 +29,9 @@ height: 100%;
 
 <div id="Container"
      style="padding-bottom:56.25%; position:relative; display:block; width: 100%">
-     <iframe id="ViostreamIframe" width="100%" height="100%" src="
+    
 <iframe title="Analisis Data Selangor - Page 1" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiY2MyYmZiMDItYmE1Yi00ZmIxLThmMGUtNDIyYmJjNjE0Y2Y1IiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
-" frameborder="0" allowfullscreen="" style="position:absolute; top:0; left: 0"></iframe>
+   <!-- <iframe id="ViostreamIframe" width="100%" height="100%" src="" frameborder="0" allowfullscreen="" style="position:absolute; top:0; left: 0"></iframe> -->
 </div>
  <!-- <div class="embed-container">
 <iframe src="" frameborder="0" allowfullscreen="" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
