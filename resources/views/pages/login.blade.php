d16464dc410edb206c144d7c7784eb47bc186641
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-24 23:27:19 +0800

    update_warna

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -10,15 +10,14 @@
             <!-- BEGIN: Login Info -->
             <div class="hidden xl:flex flex-col min-h-screen">
                 <a href="" class="-intro-x flex items-center pt-5">
-                    <img alt="Midone - HTML Admin Template" class="w-6" src="{{ asset('dist/images/logo.svg') }}">
+                    <img alt="Midone - HTML Admin Template" class="w-6" src="{{ asset('dist/images/main_logo.png') }}">
                     <span class="text-white text-lg ml-3">
-                        Rubick
                     </span>
                 </a>
                 <div class="my-auto">
-                    <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="{{ asset('dist/images/illustration.svg') }}">
-                    <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">A few more clicks to <br> sign in to your account.</div>
-                    <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Manage all your e-commerce accounts in one place</div>
+                    <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="{{ asset('dist/images/logo.png') }}">
+                    <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">Sistem Pengurusan Data Pemilih</div>
+                    <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Kawasan Parlimen Titiwangsa</div>
                 </div>
             </div>
             <!-- END: Login Info -->
