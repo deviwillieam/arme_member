25fb7ff2b467412ef3252af9280ddf44dfeba1d9
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-26 11:31:11 +0800

    update Page 2

---
+++ resources/views/pages/dashboard-tambah2.blade.php
@@ -0,0 +1,9 @@
+@extends('../layout/' . $layout)
+
+@section('subhead')
+    <title>Dashboard - Midone - Tailwind HTML Admin Template</title>
+@endsection
+
+@section('subcontent')
+No Data
+@ensdesction
