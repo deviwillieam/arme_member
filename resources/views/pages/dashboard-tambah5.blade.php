23e590e2e2ff280f181171187dedad7e6aa8bc2c
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-08-26 11:18:45 +0800

    update page 5

--- resources/views/pages/dashboard-tambah5.blade.php
+++ resources/views/pages/dashboard-tambah5.blade.php
@@ -1,11 +1,11 @@
 @extends('../layout/' . $layout)
-   2 
-   3 @section('subhead')
-   4     <title>Tambah Data</title>
-   5 @endsection
-   6 
-   7 @section('subcontent')
-   8 <!-- 
+    
+   @section('subhead')
+       <title>Tambah Data</title>
+   @endsection
+    
+   @section('subcontent')
+   <!-- 
    9  <style>.embed-container {
   10 position: relative;
   11 padding-bottom: 70.75%;
@@ -38,15 +38,12 @@
 <button onclick="myFunction()">Copy form link</button>
 
 
-  33 <marquee direction="right" 
-  34         behavior="alternate" 
-  35         style="border:Red 1px SOLID">
-  36         ⋆·˚ ༘ *ೃ⁀➷✧.*ੈ✩‧₊˚˚<b> Data will be update automatically on <i>12:00 PM</i>, If data not update please click refresh in Power BI Dashboard </b>⋆·˚ ༘ *ೃ⁀➷✧.*ੈ✩‧₊˚˚
-  37     </marquee>
-  38 
-  39     <br>
-  40     <br>
-  41 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfRG3KN1TwwhHthBei7PAX5DgLIg9nHUXuXOyio5T8YPt0sw/viewform?embedded=true" width="100%" height="1818" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-  42 
-  43 
-  44 @endsection
+  <marquee direction="right" 
+          behavior="alternate" 
+        style="border:Red 1px SOLID">
+       ⋆·˚ ༘ *ೃ⁀➷✧.*ੈ✩‧₊˚˚<b> Data will be update automatically on <i>12:00 PM</i>, If data not update please click refresh in Power BI Dashboard </b>⋆·˚ ༘ *ೃ⁀➷✧.*ੈ✩‧₊˚˚
+   </marquee>
+     <br>
+         <br>
+         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfRG3KN1TwwhHthBei7PAX5DgLIg9nHUXuXOyio5T8YPt0sw/viewform?embedded=true" width="100%" height="1818" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  @endsection
