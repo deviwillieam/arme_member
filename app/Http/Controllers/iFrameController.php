14d18f485b0700f367bbc8ddc3c62f036801ccda
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-09-06 23:21:56 +0800

    update

--- app/Http/Controllers/iFrameController.php
+++ app/Http/Controllers/iFrameController.php
@@ -29,6 +29,7 @@ class iFrameController extends Controller
         echo "Record inserted successfully.<br/>";
         echo '<a href = "iframe_edit">Click Here</a> to go back.';
     }
+    
 
     public function index()
     {
