f3d8b25657dd38f40817ec8a0fd75f26a054277c
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2022-09-07 19:37:26 +0800

    update

--- resources/views/pages/dashboard-overview-1.blade.php
+++ resources/views/pages/dashboard-overview-1.blade.php
@@ -164,7 +164,7 @@
 <div class="embed-container">
   <div id="content">
     <div style="height:580px;width:960px">
-      <iframe width="1920" height="1080" src="{{$user->dashboard}}?isMobile=true" frameborder="0" height="100%" width="100%" style="position:absolute; clip:rect(0px,1920px,750px,0px);
+      <iframe width="1920" height="1080" src="{{$user->dashboard}}isMobile=true" frameborder="0" height="100%" width="100%" style="position:absolute; clip:rect(0px,1920px,750px,0px);
             bottom:-0px; allowFullScreen=" true"></iframe>
     </div>
   </div>
